879c14bccdb9bdf501fdd950e2fe043b51a21f65
gaspard-louis.charles <gaspard-louis.charles@metronlab.com>
2020-09-26 11:56:43 +0200

    Added constants to main

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
 """Main Module"""
+
+# Plot Params
+LARGE = 22
+MEDIUM = 16
+SMALL = 12
+params = {
+    "axes.titlesize": LARGE,
+    "legend.fontsize": MEDIUM,
+    "figure.figsize": (30, 15),
+    "axes.labelsize": LARGE,
+    "xtick.labelsize": MEDIUM,
+    "ytick.labelsize": MEDIUM,
+    "figure.titlesize": LARGE,
+}
+
+REGIONS = [
+    "Auvergne-Rhône-Alpes",
+    "Bourgogne-Franche-Comté",
+    "Bretagne",
+    "Centre-Val de Loire",
+    "Corse",
+    "Grand-Est",
+    "Hauts-de-France",
+    "Ile-de-France",
+    "Normandie",
+    "Nouvelle-Aquitaine",
+    "Occitanie",
+    "Pays de la Loire",
+    "Provence-Alpes-Côte d'Azur",
+]
+
+CODE_REGIONS = {
+    11: "Ile-de-France",
+    24: "Centre-Val de Loire",
+    27: "Bourgogne-Franche-Comté",
+    28: "Normandie",
+    32: "Hauts-de-France",
+    44: "Grand-Est",
+    52: "Pays de la Loire",
+    53: "Bretagne",
+    75: "Nouvelle-Aquitaine",
+    76: "Occitanie",
+    84: "Auvergne-Rhône-Alpes",
+    93: "Provence-Alpes-Côte d'Azur",
+    94: "Corse",
+}
